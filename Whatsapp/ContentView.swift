8111ea76041cfaab92824aa0377eecc598188570
peter0bassem <pero.bassem@gmail.com>
2024-08-19 23:30:16 +0300

    Initial Commit

---
+++ Whatsapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whatsapp
+//
+//  Created by iCommunity app on 19/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Whatsapp/WhatsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappApp.swift
+//  Whatsapp
+//
+//  Created by iCommunity app on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
